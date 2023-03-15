59b15c65b12877db7e56383d939bfc9f298ef19b
Santyru <santiago0988@hotmail.com>
2023-03-15 15:39:25 -0300

    add new function

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const library = [];
+const bookTable = document.getElementById('book-table');
 
 // create a book object constructor that has title, author, pages and read as properties
 function Book(title, author, pageNumber, read) {
@@ -12,6 +13,28 @@ function addBookToLibrary(book) {
   library.push(book);
 }
 
-const juancitoElAsesino = new Book('juancito', 'elasesitno', 20, true);
-addBookToLibrary(juancitoElAsesino);
-console.log(library);
+// add a function that adds all the book objects from library into html
+// as new table rows
+function loopThroughList(list) {
+  for (const element of list) {
+    console.log(element);
+  }
+}
+
+function addBookToTable() {
+  return NaN;
+}
+
+// function that creates a new table row with Book object information as td
+function createBookTableRow(book) {
+  const row = bookTable.insertRow();
+
+  Object.keys(book).forEach((key) => {
+    const cell = document.createElement('td');
+    cell.textContent = book[key];
+    row.appendChild(cell);
+  });
+}
+
+const testBook = new Book('title1', 'author1', 1, true);
+createBookTableRow(testBook);
