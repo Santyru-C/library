ac17e706507f11bb9636b2be6ba54d31b980d494
Santyru <santiago0988@hotmail.com>
2023-03-16 14:25:38 -0300

    add new function

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const library = [];
+const mainContainer = document.getElementById('container')
 const bookTable = document.getElementById('book-table');
-
+const addBookButton = document.getElementById('add-book-button')
+const bookForm = document.getElementsByClassName('form-container')[0]
+const submitBookButton = document.getElementById('submit-book-button')
 // create a book object constructor that has title, author, pages and read as properties
 function Book(title, author, pageNumber, read) {
   this.title = title;
@@ -13,19 +16,6 @@ function addBookToLibrary(book) {
   library.push(book);
 }
 
-// add a function that adds all the book objects from library into html
-// as new table rows
-function loopThroughList(list) {
-  for (const element of list) {
-    console.log(element);
-  }
-}
-
-function addBookToTable() {
-  return NaN;
-}
-
-// function that creates a new table row with Book object information as td
 function createBookTableRow(book) {
   const row = bookTable.insertRow();
 
@@ -36,5 +26,35 @@ function createBookTableRow(book) {
   });
 }
 
-const testBook = new Book('title1', 'author1', 1, true);
-createBookTableRow(testBook);
+// add a function that adds all the book objects from library into html
+// as new table rows
+function displayAllBooks(list) {
+  list.forEach((item) => createBookTableRow(item));
+}
+
+function addBookToLibrary() {
+  bookForm.classList.add("form-container--active")
+  console.log("book")
+}
+
+function getBookFromInput(event) {
+  event.preventDefault()
+  const titleInput = document.getElementById('title-input').value;
+  const authorInput = document.getElementById('author-input').value;
+  const pagesInput = document.getElementById('pages-input').value;
+  const readInput = document.getElementById('read-input').checked;
+}
+
+submitBookButton.addEventListener('click', getBookFromInput )
+addBookButton.addEventListener('click', addBookToLibrary)
+
+
+const testBook1 = new Book('title1', 'author1', 1, true);
+const testBook2 = new Book('title2', 'author2', 2, true);
+const testBook3 = new Book('title3', 'author3', 3, true);
+library.push(testBook1);
+library.push(testBook2);
+library.push(testBook3);
+
+displayAllBooks(library);
+
