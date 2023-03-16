9e303d904b8931082c5073ba8f2487e95ae92e6b
Santyru <santiago0988@hotmail.com>
2023-03-16 14:35:20 -0300

    solve bug

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const library = [];
-const mainContainer = document.getElementById('container')
+const mainContainer = document.getElementById('container');
 const bookTable = document.getElementById('book-table');
-const addBookButton = document.getElementById('add-book-button')
-const bookForm = document.getElementsByClassName('form-container')[0]
-const submitBookButton = document.getElementById('submit-book-button')
+const addBookButton = document.getElementById('add-book-button');
+const bookForm = document.getElementsByClassName('form-container')[0];
+const submitBookButton = document.getElementById('submit-book-button');
 // create a book object constructor that has title, author, pages and read as properties
+
 function Book(title, author, pageNumber, read) {
   this.title = title;
   this.author = author;
@@ -14,6 +15,7 @@ function Book(title, author, pageNumber, read) {
 
 function addBookToLibrary(book) {
   library.push(book);
+  console.log(book);
 }
 
 function createBookTableRow(book) {
@@ -28,33 +30,28 @@ function createBookTableRow(book) {
 
 // add a function that adds all the book objects from library into html
 // as new table rows
-function displayAllBooks(list) {
-  list.forEach((item) => createBookTableRow(item));
+function displayAllBooks() {
+  library.forEach((item) => createBookTableRow(item));
 }
 
-function addBookToLibrary() {
-  bookForm.classList.add("form-container--active")
-  console.log("book")
+function showBookForm() {
+  bookForm.classList.add('form-container--active');
 }
 
 function getBookFromInput(event) {
-  event.preventDefault()
+  event.preventDefault();
   const titleInput = document.getElementById('title-input').value;
   const authorInput = document.getElementById('author-input').value;
   const pagesInput = document.getElementById('pages-input').value;
   const readInput = document.getElementById('read-input').checked;
+
+  newBook = new Book(titleInput, authorInput, pagesInput, readInput);
+
+  addBookToLibrary(newBook);
+  displayAllBooks();
 }
 
-submitBookButton.addEventListener('click', getBookFromInput )
-addBookButton.addEventListener('click', addBookToLibrary)
-
-
-const testBook1 = new Book('title1', 'author1', 1, true);
-const testBook2 = new Book('title2', 'author2', 2, true);
-const testBook3 = new Book('title3', 'author3', 3, true);
-library.push(testBook1);
-library.push(testBook2);
-library.push(testBook3);
+submitBookButton.addEventListener('click', getBookFromInput);
+addBookButton.addEventListener('click', showBookForm);
 
 displayAllBooks(library);
-
