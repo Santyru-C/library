422dfe2b1a0930588d06add578f6b8c4f623ae88
Santyru <santiago0988@hotmail.com>
2023-03-17 14:37:08 -0300

    add new function

--- script.js
+++ script.js
@@ -21,13 +21,21 @@ function removeFromList(bookRow) {
   delete library[indexToRemove];
   library = library.filter((el) => el != null); // remove empty indexes
 }
-// add functionality to the button
-// I'll have to remove the tr it is contained in after being clicked
+
 function removeBook() {
   const parentRow = this.parentNode.parentNode;
-  parentRow.remove();
-
   removeFromList(parentRow);
+  parentRow.remove();
+}
+
+// refactor next two functions into one
+function createReadButton(value) {
+  const removeButton = document.createElement('button');
+  removeButton.textContent = value;
+  removeButton.classList.add('read-button');
+  // removeButton.addEventListener('click', removeBook);
+
+  return document.createElement('td').appendChild(removeButton);
 }
 
 function createRemoveButton() {
@@ -43,13 +51,20 @@ function setDataAttribute(row, book) {
   const bookIndex = library.indexOf(book);
   row.setAttribute('data-index', bookIndex.toString());
 }
+
 function createBookTableRow(book) {
   const row = bookTable.insertRow();
   setDataAttribute(row, book);
 
   Object.keys(book).forEach((key) => {
     const cell = document.createElement('td');
-    cell.textContent = book[key];
+
+    if (key !== 'read') {
+      cell.textContent = book[key];
+    } else {
+      cell.appendChild(createReadButton(book[key]));
+    }
+
     row.appendChild(cell);
   });
 
