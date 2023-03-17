6999f1c010b13dffd30d6fafba228c989f7a8d22
Santyru <santiago0988@hotmail.com>
2023-03-17 13:14:22 -0300

    add data-attributes to td

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const library = [];
+
 const bookTable = document.getElementById('book-table');
 const addBookButton = document.getElementById('add-book-button');
 const formContainer = document.getElementsByClassName('form-container')[0];
 const form = document.getElementsByTagName('form')[0];
-// create a book object constructor that has title, author, pages and read as properties
 
-function Book(title, author, pageNumber, read) {
+function Book(id, title, author, pageNumber, read) {
+  this.id = id;
   this.title = title;
   this.author = author;
   this.pageNumber = pageNumber;
@@ -19,7 +20,7 @@ function addBookToLibrary(book) {
 // add functionality to the button
 // I'll have to remove the tr it is contained in after being clicked
 function removeBook() {
-  console.log("I'm being removed!");
+  this.parentNode.parentNode.remove();
 }
 
 function createRemoveButton() {
@@ -30,8 +31,13 @@ function createRemoveButton() {
   return document.createElement('td').appendChild(removeButton);
 }
 
+function setDataAttribute(row, book) {
+  const bookIndex = library.indexOf(book);
+  row.setAttribute('data-index', bookIndex.toString());
+}
 function createBookTableRow(book) {
   const row = bookTable.insertRow();
+  setDataAttribute(row, book);
 
   Object.keys(book).forEach((key) => {
     const cell = document.createElement('td');
