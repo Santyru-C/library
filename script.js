c4375bac8491e966a580a970153947d9ae72f11a
Santyru <santiago0988@hotmail.com>
2023-03-21 15:04:14 -0300

    refactor removeFromList

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const Library = {
   displayAllBooks() {
     this.storage.forEach((item) => createBookTableRow(item));
   },
+  removeFromStorage(index) {
+    delete Library.storage[index];
+    this.storage = this.storage.filter((el) => el != null); // remove empty indexes
+  },
 };
 
 function Book(title, author, pageNumber, read) {
@@ -23,14 +27,9 @@ function getBookIndexFromRow(bookRow) {
   return indexFromRow;
 }
 
-function removeFromList(indexToRemove) {
-  delete library[indexToRemove];
-  library = library.filter((el) => el != null); // remove empty indexes
-}
-
 function removeBook() {
   const bookRow = this.parentNode.parentNode;
-  removeFromList(getBookIndexFromRow(bookRow));
+  Library.removeFromStorage(getBookIndexFromRow(bookRow));
   bookRow.remove();
 }
 
@@ -65,7 +64,7 @@ function createRemoveButton() {
 }
 
 function setDataAttribute(row, book) {
-  const bookIndex = library.indexOf(book);
+  const bookIndex = Library.storage.indexOf(book);
   row.setAttribute('data-index', bookIndex.toString());
 }
 
@@ -120,4 +119,4 @@ const default3 = new Book('Hamlet', 'William Shakespeare', 1, 'Yes');
 Library.addBook(default1);
 Library.addBook(default2);
 Library.addBook(default3);
-displayAllBooks(Library.storage);
+Library.displayAllBooks();
